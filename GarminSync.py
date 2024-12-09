ab7139e1e4be2491cb533806c300341dcf54c3ef
kvnZero <abigeater@163.com>
2024-12-09 11:04:46 +0800

    feat garmin data sync

---
+++ GarminSync.py
@@ -0,0 +1,74 @@
+import base64
+import os.path
+import requests
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_v1_5
+from garth.http import Client
+import zipfile
+
+def encrpt(password, public_key):
+    rsa = RSA.importKey(public_key)
+    cipher = PKCS1_v1_5.new(rsa)
+    return base64.b64encode(cipher.encrypt(password.encode())).decode()
+
+def syncData(garmin_email = None, garmin_password = None):
+
+    global_garth = Client()
+    garth = Client()
+
+    garth.configure(domain="garmin.cn")
+
+    try:
+
+        garth.login(garmin_email, garmin_password)
+        global_garth.login(garmin_email, garmin_password)
+
+    except Exception:
+
+        print("登录态失败")
+        return False
+
+    global_activities = global_garth.connectapi(
+        f"/activitylist-service/activities/search/activities",
+        params={"activityType": "running", "limit": 5, "start": 0, 'excludeChildren': False},
+    )
+    activities = garth.connectapi(
+        f"/activitylist-service/activities/search/activities",
+        params={"activityType": "running", "limit": 5, "start": 0, 'excludeChildren': False},
+    )
+
+    add_list = []
+    has_exist = []
+    for item2 in global_activities:
+        has_exist.append(item2['startTimeGMT'])
+
+    for item in activities:
+        if item['startTimeGMT'] not in has_exist:
+            rid = item['activityId']
+            rid = str(rid)
+
+            add_list.append(rid)
+            if not os.path.isfile(rid+".zip"):
+                res = garth.download(
+                    f"/download-service/files/activity/{rid}",
+                )
+                with open(rid+".zip", "wb") as f:
+                    f.write(res)
+                with zipfile.ZipFile(rid + ".zip", 'r') as zip_ref:
+                    zip_ref.extractall(rid)
+        else:
+            print("%s:已同步" % item['activityId'])
+
+    print(add_list)
+    print("本次同步数量：%s" % len(add_list))
+    for u in add_list:
+        with open(u + "/" + u + "_ACTIVITY.fit", 'rb') as fd:
+            uploaded = requests.post('https://connectapi.garmin.com/upload-service/upload',
+                          files={'file': fd},
+                          headers={'authorization': global_garth.oauth2_token.__str__()})
+            # uploaded = global_garth.upload(fd)
+            print(uploaded.content)
+
+    return True
+
+activity = syncData(os.getenv("GARMIN_EMAIL"), os.getenv("GARMIN_PASSWORD"))
