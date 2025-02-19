eaecc50626984956d01801fcf33689620b4f0b13
Lin <abigeater@163.com>
2025-02-19 22:14:15 +0800

    打印错误原因

--- GarminSync.py
+++ GarminSync.py
@@ -24,8 +24,9 @@ def syncData(garmin_email, garmin_password):
     try:
         garth.login(garmin_email, garmin_password)
         global_garth.login(garmin_email, garmin_password)
-    except Exception:
+    except Exception e:
         print("登录态失败")
+        print(e)
         return False
 
     global_activities = global_garth.connectapi(
